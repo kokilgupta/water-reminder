e53eeff5ffb4cbcd63eb464ceb535fba51b5469d
Kokil Gupta <kokil.gupta@walmart.com>
2025-11-18 18:54:46 +0530

    initial commit

---
+++ Water Reminder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Water Reminder
+//
+//  Created by Kokil Gupta on 18/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, kokil")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Water Reminder/Water_ReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Water_ReminderApp.swift
+//  Water Reminder
+//
+//  Created by Kokil Gupta on 18/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Water_ReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
